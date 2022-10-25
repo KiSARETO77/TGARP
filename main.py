5d45759e8734914be2583f693a55bcb380aa419f
KiSARETO77 <106799027+KiSARETO77@users.noreply.github.com>
2022-10-25 11:16:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from aminofix.lib.util.exceptions import IpTemporaryBan
-from aminofix import Client,SubClient
-from samino import Client,Local
 from time import sleep as ZZ
 from os import system as Z
+import aminofix
+import samino
 #############################################
 Z("clear")
-C=Client(proxies={"https":"http://dimatjasko10:KWJYs68q@185.112.13.43:2831"})
-Ç=Client(proxies={"https":"http://dimatjasko10:KWJYs68q@185.112.13.43:2831"})
+C=samino.Client(proxies={"https":"http://dimatjasko10:KWJYs68q@185.112.13.43:2831"})
+Ç=aminofix.Client(proxies={"https":"http://dimatjasko10:KWJYs68q@185.112.13.43:2831"})
 Ç.login("pb-@digdig.org","GOKU12##")
 C.login("pb-@digdig.org","GOKU12##")
-S=Local(comId=3434136,proxies={"https":"http://dimatjasko10:KWJYs68q@185.112.13.43:2831"})
-q,p,Q=0,100,0
+S=samino.Local(comId=3434136,proxies={"https":"http://dimatjasko10:KWJYs68q@185.112.13.43:2831"})
+Q=0
+q=0
+p=100
 PI=[None,1,"1","None"]
 while 1>0:
 	M=S.get_online_users(start=q,size=p)
@@ -24,8 +26,10 @@ while 1>0:
 				Q+=1
 				Ç.invite_to_chat(chatId="1cddf160-d5b9-4b42-b8f3-6701eb630e3a",userId=U)
 				print(f"- {Q} Done invite {N} -{L}")
-			except Exception as F:
-				print(F)
+			except IpTemporaryBan:
 				print(f"Wait For 60 Second")
 				ZZ(60)
+				pass
+			except Exception as F:
+				print(F)
 				pass
