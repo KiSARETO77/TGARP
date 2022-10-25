8aca4093262f55e4289dcad0de8e2fe0f3d933ae
KiSARETO77 <106799027+KiSARETO77@users.noreply.github.com>
2022-10-25 11:10:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from time import sleep as ZZ
 from os import system as Z
 #############################################
 Z("clear")
+C=Client(proxies={"https":"http://dimatjasko10:KWJYs68q@185.112.13.43:2831"})
 Ç=Client(proxies={"https":"http://dimatjasko10:KWJYs68q@185.112.13.43:2831"})
-C,O=3434136,"1cddf160-d5b9-4b42-b8f3-6701eb630e3a"
+S=Local(comId=3434136,proxies={"https":"http://dimatjasko10:KWJYs68q@185.112.13.43:2831"})
 q,p,Q=0,100,0
 PI=[None,1,"1","None"]
 while 1>0:
 	Ç.login("pb-@digdig.org","GOKU12##")
-	S=Local(comId=C,proxies={"https":"http://dimatjasko10:KWJYs68q@185.112.13.43:2831"})
+	C.login("pb-@digdig.org","GOKU12##")
 	M=S.get_online_users(start=q,size=p)
 	q+=100
 	p+=200
@@ -21,13 +22,10 @@ while 1>0:
 		if P in PI:
 			try:
 				Q+=1
-				S=Local(comId=0,proxies={"https":"http://dimatjasko10:KWJYs68q@185.112.13.43:2831"})
-				S.invite_to_chat(chatId=O,userId=U)
+				Ç.invite_to_chat(chatId="1cddf160-d5b9-4b42-b8f3-6701eb630e3a",userId=U)
 				print(f"- {Q} Done invite {N} -{L}")
-			except IpTemporaryBan:
-				print(f"Wait For 60 Second")
-				ZZ(60)
-				pass
 			except Exception as F:
 				print(F)
+				print(f"Wait For 60 Second")
+				ZZ(60)
 				pass
